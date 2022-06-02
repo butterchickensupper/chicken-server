0462237b4fa2f105789d17caeb946bd67d6e595d
Butter Chicken <butterchickensupper@gmail.com>
2022-06-02 03:58:03 +0000

    updates

--- provider.tf
+++ provider.tf
@@ -9,5 +9,48 @@ provider "aws" {
 
   endpoints {
     dynamodb = "http://localhost:4566"
+    lambda   = "http://localhost:4574"
+    iam      = "http://localhost:4593"
   }
+}
+
+resource "aws_lambda_layer_version" "example" {
+  layer_name          = "lambda_layer_name"
+  compatible_runtimes = ["nodejs14.x"]
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "src/services/index.js"
+  output_path = "lambda_function.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "lambda_function.zip"
+  function_name    = "test_lambda"
+  role             = aws_iam_role.iam_for_lambda_tf.arn
+  handler          = "index.handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "nodejs14.x"
+  layers           = [aws_lambda_layer_version.example.arn]
+}
+
+resource "aws_iam_role" "iam_for_lambda_tf" {
+  name = "iam_for_lambda_tf"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
