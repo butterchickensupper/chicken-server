2b6308a46a506c2233bcd461955d332689a4a2ae
Butter Chicken <butterchickensupper@gmail.com>
2022-06-05 02:17:45 +0000

    updates

---
+++ terraform/modules/lambda/lamdba.tf
@@ -0,0 +1,56 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "../src/services/index.js"
+  output_path = "service.zip"
+}
+
+data "archive_file" "hello_lambda_zip" {
+  type        = "zip"
+  source_file = "../src/services/hello.js"
+  output_path = "hello.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "service.zip"
+  function_name    = "test_lambda"
+  role             = aws_iam_role.iam_for_lambda_tf.arn
+  handler          = "index.handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "nodejs14.x"
+}
+
+resource "aws_lambda_function" "test_hello_lambda" {
+  filename         = "hello.zip"
+  function_name    = "test_hello_lambda"
+  role             = aws_iam_role.iam_for_lambda_tf.arn
+  handler          = "hello.handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "nodejs14.x"
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "aws/lambda/${aws_lambda_function.test_hello_lambda.function_name}"
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "iam_for_lambda_tf" {
+  name = "iam_for_lambda_tf"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.iam_for_lambda_tf.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,57 +14,10 @@ provider "aws" {
   }
 }
 
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_file = "../src/services/index.js"
-  output_path = "service.zip"
+module "api_gateway" {
+  source = "./modules/api_gateway"
 }
 
-data "archive_file" "hello_lambda_zip" {
-  type        = "zip"
-  source_file = "../src/services/hello.js"
-  output_path = "hello.zip"
-}
-
-resource "aws_lambda_function" "test_lambda" {
-  filename         = "service.zip"
-  function_name    = "test_lambda"
-  role             = aws_iam_role.iam_for_lambda_tf.arn
-  handler          = "index.handler"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime          = "nodejs14.x"
-}
-
-resource "aws_lambda_function" "test_hello_lambda" {
-  filename         = "hello.zip"
-  function_name    = "test_hello_lambda"
-  role             = aws_iam_role.iam_for_lambda_tf.arn
-  handler          = "hello.handler"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime          = "nodejs14.x"
-}
-
-resource "aws_iam_role" "iam_for_lambda_tf" {
-  name = "iam_for_lambda_tf"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.iam_for_lambda_tf.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+module "lambda" {
+  source = "./modules/lambda"
 }
