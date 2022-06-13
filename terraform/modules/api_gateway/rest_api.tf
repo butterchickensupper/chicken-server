31ed1658f23fd786be24b4c7af0d067d5a7c1b19
Butter Chicken <butterchickensupper@gmail.com>
2022-06-13 00:08:01 +0000

    updates

--- terraform/modules/api_gateway/rest_api.tf
+++ terraform/modules/api_gateway/rest_api.tf
@@ -11,20 +11,13 @@ resource "aws_api_gateway_rest_api" "rest_api" {
 resource "aws_api_gateway_resource" "rest_api_resource" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = "movies"
+  path_part   = "menu"
 }
 
 resource "aws_api_gateway_method" "rest_api_get_method" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   resource_id   = aws_api_gateway_resource.rest_api_resource.id
   http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method" "rest_api_post_method" {
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_resource.rest_api_resource.id
-  http_method   = "POST"
   authorization = "NONE"
 }
 
