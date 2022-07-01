7c66459c7d92459dd15e7af133fb632a2ce911e0
Butter Chicken <butterchickensupper@gmail.com>
2022-07-01 20:48:56 +0000

    tweaks

--- functions/stock-seller/app.js
+++ functions/stock-seller/app.js
@@ -1,30 +1,45 @@
-const crypto = require("crypto");
+const dynamodb = require('aws-sdk/clients/dynamodb');
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max)) + 1;
-}
+const tableName = process.env.SAMPLE_TABLE;
+const docClient = process.env.IS_OFFLINE === 'true' 
+    ? new dynamodb.DocumentClient({ endpoint: 'http://ddb_local:8000' })
+    : new dynamodb.DocumentClient();
 
 /**
- * Sample Lambda function which mocks the operation of selling a random number of shares for a stock.
- * For demonstration purposes, this Lambda function does not actually perform any  actual transactions. It simply returns a mocked result.
+ * Gets the menu from dynamo
  * 
  * @param {Object} event - Input event to the Lambda function
  * @param {Object} context - Lambda Context runtime methods and attributes
  *
- * @returns {Object} object - Object containing details of the stock selling transaction
+ * @returns {Object} object - Object containing details of the menu
  * 
  */
 exports.lambdaHandler = async (event, context) => {
-    // Get the price of the stock provided as input
-    stock_price = event["stock_price"]
-    var date = new Date();
-    // Mocked result of a stock selling transaction
-    let transaction_result = {
-        'id': crypto.randomBytes(16).toString("hex"), // Unique ID for the transaction
-        'price': stock_price.toString(), // Price of each share
-        'type': "sell", // Type of transaction(buy/ sell)
-        'qty': getRandomInt(10).toString(),  // Number of shares bought / sold(We are mocking this as a random integer between 1 and 10)
-        'timestamp': date.toISOString(),  // Timestamp of the when the transaction was completed
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getMenuHandler only accept GET method, you tried: ${event.httpMethod}`);
     }
-    return transaction_result
+    // All log statements are written to CloudWatch
+    console.info('received:', [event]);
+
+    var params = {
+        TableName : tableName,
+        Key: { partitionKey: 'dev|menu', rangeKey: 'menu' }
+    };
+    const data = await docClient.get(params).promise();
+    console.info('data', [data]);
+    if (!data?.Item?.objectData) {
+        return {
+            statusCode: 404,
+            body: JSON.stringify({ message: 'failed to find menu'})
+        };
+    } 
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(data?.Item?.objectData)
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
 };
